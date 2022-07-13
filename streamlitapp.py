59e12d557e984f993b87014a2eb44f1e72817cdc
Muwahiddin Massaid <103725533+massaid01@users.noreply.github.com>
2022-07-13 17:15:42 +0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -48,7 +48,7 @@ Vintage = st.number_input('How long you have been a our customers (in a day)', m
 features = {'Gender': Gender, 'Age': Age,
             'Driving_License': Driving_License, 'Region_Code': Region_Code,
             'Previously_Insured' : Previously_Insured, 'Vehicle_Age': Vehicle_Age, 
-            'Vehicle_Damage': Vehicle_Damage, 'Annual_Premium ':Annual_Premium, 
+            'Vehicle_Damage': Vehicle_Damage, 'Annual_Premium':Annual_Premium, 
             'Policy_Sales_Channel': Policy_Sales_Channel, 'Vintage': Vintage
             }
 
